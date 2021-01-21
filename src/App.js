8414731a0de5c42336af3d64355c732ab2fc91fb
Gaurav Agarwal <gauravggg21@gmail.com>
2021-01-21 23:43:21 +0530

    Completed Final APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
+import Background from "/public/web-pirate_00435677.jpg";
+
+const flags = {
+  "🏴‍☠️": "Pirate",
+  "🇮🇳": "Indian",
+  "🇯🇵": "Japan",
+  "🇳🇱": "Netherlands",
+  "🇬🇧": "United Kingdom",
+  "🇮🇹": "Italy",
+  "🇳🇵": "Nepal"
+};
+
+export default function App() {
+  const [selectedFlag, setFlag] = useState("");
+  const [flagName, setFlagName] = useState("");
+
+  useEffect(() => {
+    let flag = "This land be not yet discovered";
+    if (selectedFlag) {
+      flag = flags[selectedFlag] || flag;
+    } else {
+      flag = "";
+    }
+    setFlagName(flag);
+  }, [selectedFlag]);
+
+  const displayFlagName = (flag) => {
+    setFlag(flag);
+  };
+
+  const inputChange = (event) => {
+    const flag = event.target.value;
+    setFlag(flag);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${Background})`,
+        width: "100vw",
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        margin: 0
+      }}
+    >
+      <div className="container">
+        <h1>Flag Interpreter</h1>
+        <input
+          id="flagInput"
+          className="mb-20"
+          placeholder="Enter flag"
+          value={selectedFlag}
+          onChange={inputChange}
+        />
+
+        <h3>Flag : {flagName}</h3>
+
+        <h2 style={{ marginTop: "20px", marginBottom: "20px" }}>
+          List of flags
+        </h2>
+        {Object.keys(flags).map((flag) => {
+          return (
+            <button
+              key={flag}
+              onClick={() => displayFlagName(flag)}
+              style={{
+                fontSize: "2rem",
+                border: "0px",
+                background: "transparent",
+                cursor: "pointer"
+              }}
+            >
+              {flag}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
